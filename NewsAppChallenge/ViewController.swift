efdc4fba30b499487f12649bda827d9c5bbb7ffb
samuel mejia <samg2583@hotmail.com>
2018-09-03 14:43:20 -0500

    Initial Commit

---
+++ NewsAppChallenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NewsAppChallenge
+//
+//  Created by Samuel Mejia Guarin on 3/09/18.
+//  Copyright © 2018 Samuel Mejia Guarin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
