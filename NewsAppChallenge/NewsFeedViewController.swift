a5c242bafab3b8e3137ee37171bee2662b99b676
samuel mejia <samg2583@hotmail.com>
2018-09-05 08:28:44 -0500

    Pods

---
+++ NewsAppChallenge/NewsFeedViewController.swift
@@ -0,0 +1,25 @@
+//
+//  NewsFeedController.swift
+//  NewsAppChallenge
+//
+//  Created by Samuel Mejia Guarin on 3/09/18.
+//  Copyright © 2018 Samuel Mejia Guarin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class NewsFeedViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
---
+++ Podfile
@@ -0,0 +1,16 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '9.0'
+
+target 'NewsAppChallenge' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for NewsAppChallenge
+  pod 'SwiftyJSON'
+  pod 'Alamofire'
+  pod  'AlamofireObjectMapper'
+  pod  'ObjectMapper'
+  pod 'SDWebImage'
+  pod 'Spring', :git => 'https://github.com/MengTo/Spring.git'
+
+end
